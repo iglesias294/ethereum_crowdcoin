e90fc329d9afe2c99a9795c8673cd8127b52bcc6
Arturo <iglesias294@icloud.com>
2018-03-31 17:30:27 +0800

    added requests section

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0xD716c84447e2aC7865b15Aec2a73D527fe7bF1b4'
+  '0x6eaF701F99A3f87e81D20811d79A1A61ee8aCeda'
 );
 
 export default instance;
